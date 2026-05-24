83765ebed8732cc4210a82318fde31d176960a48
sujit-senapati <senapatisujit7@outlook.com>
2026-05-25 03:26:14 +0530

    updated ui

--- Frontend/src/pages/Start.jsx
+++ Frontend/src/pages/Start.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import MoveOnLogo from '../assets/logos/MoveOnLogo.png'
+import coverPhoto from '../assets/cover/cover_photo.avif'
 
 const Start = () => {
   console.log('Home page loaded')
   return (
     <div className='min-h-screen bg-slate-800 flex items-center justify-center p-4'>
-      <div className='w-full max-w-md bg-zinc-100 rounded-3xl shadow-xl overflow-hidden'>
+      <div className='w-full max-w-md bg-white rounded-3xl shadow-xl overflow-hidden'>
         <div className='bg-cover bg-center min-h-[90vh] w-full flex justify-between flex-col pt-8'>
           <img className='w-32 -mt-5' src={MoveOnLogo} alt="" />
-          <div className='bg-white pb-7 py-4 px-4'>
+          <div className='bg-white pb-7 py-4 px-4 '>
+            <img src={coverPhoto} alt="" />
             <h2 className='text-3xl font-bold'>Get started with MoveOn</h2>
             <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5 active:scale-95'>Continue</Link>
           </div>
